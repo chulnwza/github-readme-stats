e2446d1a55e8fe4c3b76d9a68a692d7160b62af9
chayatorn <chayatornchuonwork@gmail.com>
2026-07-20 17:50:53 +0700

    fix: include forked repos in top languages query

--- src/fetchers/top-languages.js
+++ src/fetchers/top-languages.js
@@ -20,8 +20,8 @@ const fetcher = (variables, token) => {
       query: `
       query userInfo($login: String!) {
         user(login: $login) {
-          # fetch only owner repos & not forks
-          repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
+          # fetch owner repos including forks
+          repositories(ownerAffiliations: OWNER, first: 100) {
             nodes {
               name
               languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
